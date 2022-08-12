465a688542406855ba4aca273f367e6fa8975a74
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 08:05:35 -0700

    Added Quotient and Remainder problem

---
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functional Core Programming");
+            Console.WriteLine("Enter 1 for Quotient Remainder");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    QuotientRemainder Result = new QuotientRemainder();
+                    break;
+                default:
+                    Console.WriteLine("Enter the Valid Number");
+                    break;
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/FunctionalProgramming/QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class QuotientRemainder
+    {
+        public QuotientRemainder()
+        {
+            Console.WriteLine("Enter the dividend");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the diviser");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+            int quotient = Num1 / Num2;
+            int remainder = Num1 % Num2;
+            Console.WriteLine("Quotient is:" + quotient);
+            Console.WriteLine("Remainder is:" + remainder);
+
+        }
+    }
+}
