35da3c8fd73017bee27acab031ac11dc1f140582
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 08:09:24 -0700

    Added Swap Number problem

--- FunctionalProgramming/FunctionalProgramming/Program.cs
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -8,11 +8,15 @@ namespace FunctionalProgramming
         {
             Console.WriteLine("Functional Core Programming");
             Console.WriteLine("Enter 1 for Quotient Remainder");
+            Console.WriteLine("Enter 2 for Swapping two numbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     QuotientRemainder Result = new QuotientRemainder();
+                    break;
+                case 2:
+                    SwapNumber NewNumber = new SwapNumber();
                     break;
                 default:
                     Console.WriteLine("Enter the Valid Number");
---
+++ FunctionalProgramming/FunctionalProgramming/SwapNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class SwapNumber
+    {
+        public SwapNumber()
+        {
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.Write("Swapped numbers are a= " + a + " b= " + b);
+        }
+    }
+}
