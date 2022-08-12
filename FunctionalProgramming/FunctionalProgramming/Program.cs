387c00ce38654782ac9e52dd8012b6e35fa3d4f3
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-12 08:19:25 -0700

    Added Vowel Consonent problem

--- FunctionalProgramming/FunctionalProgramming/Program.cs
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -10,6 +10,7 @@ namespace FunctionalProgramming
             Console.WriteLine("Enter 1 for Quotient Remainder");
             Console.WriteLine("Enter 2 for Swapping two numbers");
             Console.WriteLine("Enter 3 for to check Odd or Even");
+            Console.WriteLine("Enter 4 for to check Vowel or Consonent");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,6 +22,9 @@ namespace FunctionalProgramming
                     break;
                 case 3:
                     EvenOdd resultNew = new EvenOdd();
+                    break;
+                case 4:
+                    VowelConsonent character = new VowelConsonent();
                     break;
                 default:
                     Console.WriteLine("Enter the Valid Number");
---
+++ FunctionalProgramming/FunctionalProgramming/VowelConsonent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class VowelConsonent
+    {
+        public VowelConsonent()
+        {
+            Console.WriteLine("Enter a character:");
+            char input = Char.Parse(Console.ReadLine().ToLower());
+            if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u')
+            {
+                Console.WriteLine("It's a vowel");
+            }
+            else if (input >= 'a' && input <= 'z')
+            {
+
+                Console.WriteLine("It's a consonant");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input please enter an alphabet");
+            }
+        }
+    }
+}
